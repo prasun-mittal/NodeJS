02ca4509f649d7094b8d80c4824a0e01f3fb8faf
Prasun Mittal <prasunmittal09@gmail.com>
2026-06-23 16:40:20 +0530

    mongoDB integrated

--- Project-01 (Rest api)/index.js
+++ Project-01 (Rest api)/index.js
@@ -1,9 +1,28 @@
 const express=require('express');
 const fs=require('fs')
-const users=require('./MOCK_DATA.json');
+// const users=require('./MOCK_DATA.json');
+const mongoose=require('mongoose');
 const { log } = require('console');
 const app=express();
 const PORT=8000;
+
+// Connection of mongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/node-practice')
+.then(()=>console.log("mongoDB Connected"))
+.catch((err)=>console.log("Mongo Error",err));
+
+
+// Schema
+const userSchema = new mongoose.Schema({
+    firstName:{type: String, required:true},
+    lastName:{type:String},
+    email:{type:String, required:true, unique:true},
+    jobTitle:{type:String},
+    gender:{type:String}
+},{timestamps:true})
+
+const User = mongoose.model("user",userSchema);
+
 
 // Middleware - plugin
 app.use(express.urlencoded({extended: false}));
@@ -28,67 +47,92 @@ app.use((req,res,next)=>{
 })
 
 //ROUTES
-app.get('/api/users',(req,res)=>{
+app.get('/api/users',async (req,res)=>{
     // res.setHeader('myname',"Prasun Mittal");
     // always add X to custom header like 'X-Myname'   // Good Practice
     // console.log(req.headers);
-    
-    return res.json(users);
+    // return res.json(users);
+
+    const allUsers = await User.find({});
+    return res.json(allUsers);
 })
-app.get('/users',(req,res)=>{
+app.get('/users',async (req,res)=>{
+    // const html=`
+    //     <ul>
+    //     ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    //     </ul>
+    // `;
+    // res.send(html);
+
+    const allDbUsers=await User.find({});
     const html=`
         <ul>
-        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+        ${allDbUsers.map(user => `<li>${user.firstName} - ${user.email} </li>`).join("")}
         </ul>
     `;
     res.send(html);
 })
 
 app.route('/api/users/:id')
-.get((req,res)=>{
+.get(async(req,res)=>{
+    /*
     const id=Number(req.params.id);
     const user =users.find((user) => user.id === id);
     if(!user){
         return res.status(404).json({error:"User not found"});  // If user does not exist, return 404 Not Found
     }
     return res.json(user);
-})
-.patch((req,res) => {
-    // edit user with id
-    const id=Number(req.params.id);
-     const body =req.body;
-     const index=users.findIndex(user => user.id===id);
-    if(index === -1){
-        return res.status(404).json({
-            message:"User not found"
-        });
+    */
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return res.status(404).json({error:"User not found"});  // If user does not exist, return 404 Not Found
     }
-    users[index]={
-        ...users[index],
-        ...body
-    };
-    fs.writeFile('./MOCK_DATA.json',
-        JSON.stringify(users),
-        (err)=>{
-            return res.json({
-                status:"Success"
-            });
-    });
+    return res.json(user);
 })
-.delete((req,res)=>{
+.patch(async(req,res) => {
+    // edit user with id
+    await User.findByIdAndUpdate(req.params.id, {lastName:'Changed'})
+    return res.json({status:"Success"})
+
+    // const id=Number(req.params.id);
+    //  const body =req.body;
+    //  const index=users.findIndex(user => user.id===id);
+    // if(index === -1){
+    //     return res.status(404).json({
+    //         message:"User not found"
+    //     });
+    // }
+    // users[index]={
+    //     ...users[index],
+    //     ...body
+    // };
+    // fs.writeFile('./MOCK_DATA.json',
+    //     JSON.stringify(users),
+    //     (err)=>{
+    //         return res.json({
+    //             status:"Success"
+    //         });
+    // });
+})
+.delete(async(req,res)=>{
     // delete user with id
-    const id=Number(req.params.id);
-    const index=users.findIndex(user=>user.id===id);
-    users.splice(index,1);
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err)=>{
-        return res.json({
-            status:"Success"
-        });
-    });
+    // const allDbUsers=await User.find({});
+
+    // const id=Number(req.params.id);
+    // const index=users.findIndex(user=>user.id===id);
+    // users.splice(index,1);
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err)=>{
+    //     return res.json({
+    //         status:"Success"
+    //     });
+    // });
+
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({status:"success"})
 });
 
 
-app.post('/api/users',(req,res)=>{
+app.post('/api/users',async(req,res)=>{
 
     // Input validation
     // Check kar rahe hain ki required fields request body me present hain ya nahi.
@@ -98,10 +142,20 @@ app.post('/api/users',(req,res)=>{
     if(!body || !body.first_name || !body.email || !body.last_name || !body.gender || !body.job_title){
         return res.status(400).json({msg: "All fields are required"});
     }
-    users.push({...body, id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
-        return res.status(201).json({ status:"Success",id:users.length});   // New user successfully create hua hai, isliye 201 status code return kar rahe hain.
-    })   
+    const result = await User.create({
+        firstName:body.first_name,
+        lastName:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        jobTitle:body.job_title
+    });
+
+    return res.status(201).json({message:"Success"});
+
+    // users.push({...body, id:users.length+1});
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+    //     return res.status(201).json({ status:"Success",id:users.length});   // New user successfully create hua hai, isliye 201 status code return kar rahe hain.
+    // })   
 })
 /*
     200 → OK (GET, PATCH, DELETE)
@@ -112,4 +166,4 @@ app.post('/api/users',(req,res)=>{
 */
 
 
-app.listen(PORT,()=> console.log(`Server started at port: ${PORT}`));2
+app.listen(PORT,()=> console.log(`Server started at port: ${PORT}`));
