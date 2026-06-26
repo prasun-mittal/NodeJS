048baf4daca45e8d9232e7f0a235d0a3a15be66c
Prasun Mittal <prasunmittal09@gmail.com>
2026-06-26 21:57:40 +0530

    server side rendring & ejs

--- URL Shortener/index.js
+++ URL Shortener/index.js
@@ -1,17 +1,46 @@
 const express = require('express');
+const path=require('path');
 const app=express();
 const PORT = 8001;
 const urlRoutes = require('./routes/url')
 const {connectToMongoDb} = require('./connection')
 const URL = require('./models/url')
 
+const staticRoute = require('./routes/staticRouter');
 connectToMongoDb('mongodb://localhost:27017/short-url')
 .then(()=> console.log("MongoDb connected")
 );
 
+app.set('view engine','ejs');
+app.set('views',path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.use('/',staticRoute);
+
+/*
+app.get('/test',async (req,res)=>{
+    const allUrls= await URL.find({});
+    // return res.end(`
+    //     <html>
+    //         <head></head>
+    //         <body>
+    //             <ol>
+    //                 ${allUrls.map((url)=>`<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length} </li>`).join("")}
+    //             </ol>
+    //         </body>
+    //     </html>    
+    // `);   // this is without ejs 
+
+    return res.render('home',{
+        urls:allUrls });      // this is server side rendering
+})
+*/
 
 app.use("/url",urlRoutes);
+
 app.get('/:shortId',async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
