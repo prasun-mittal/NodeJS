c5e687ee68af9282100eb4bfec7dfde12ed546dd
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-06 18:48:44 +0530

    minor fix

--- URL Shortener/controller/user.js
+++ URL Shortener/controller/user.js
@@ -9,7 +9,7 @@ async function handleUserSignup(req,res){
         email,
         password
     });
-    return res.redirect("/");
+    return res.redirect("/login");
 }
 
 async function handleUserLogin(req,res){
